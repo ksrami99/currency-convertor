75a3617bcf94e5a2fabf8309759fa4db0aadbc7f
Karan <ksrami99@gmail.com>
2024-03-01 16:22:24 +0530

    Corrency Converot Project Completed

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatefulWidget {
+  const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  State<CurrencyConverterMaterialPage> createState() =>
+      _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState
+    extends State<CurrencyConverterMaterialPage> {
+  double result = 0;
+
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.black,
+        style: BorderStyle.solid,
+        width: 2.0,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text(
+          'Corrency Convertor',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '${result == 0 ? 0 : result.toStringAsFixed(2)} INR',
+              style: const TextStyle(
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: textEditingController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: 'Please enter the amount in USD',
+                  hintStyle: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: const Icon(
+                    Icons.attach_money,
+                  ),
+                  prefixIconColor: Colors.black,
+                  fillColor: Colors.white,
+                  filled: true,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    result = double.parse(textEditingController.text) * 82.90;
+                  });
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                child: const Text('Convert'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
